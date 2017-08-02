335db60d5f13ea0a9973a6d1fcbadf254c273a09
Carlo Battilana <carlo.battilana@gmail.com>
2017-08-02 11:06:52 +0200

    Carlo's RAW-RECO cfg

---
+++ Tools/test/crab/crab_SingleMuon_ZMu_Run2016C_cfg.py
@@ -0,0 +1,35 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'SingleMuonRun2016C_ZMu_23Sep2016'
+
+config.section_('JobType')
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = '../muonPogNtuples_cfg.py'
+#config.JobType.outputFiles = ['muonNTuple.root']
+config.JobType.pyCfgParams = ['globalTag=80X_dataRun2_2016SeptRepro_v3',
+                              'ntupleName=muonPOGNtuple_SingleMuonRun2016C_23Sep2016.root',
+                              'hasRaw=True',
+                              'nEvents=-1',
+                              'runOnMC=False',
+                              'hltPathFilter=all',
+                              'minMuPt=45.0',
+                              'minNMu=2'
+               ]
+config.JobType.allowUndistributedCMSSW = True  # To fix cmssw releases
+
+config.section_('Data')
+config.Data.inputDataset = '/SingleMuon/Run2016C-ZMu-23Sep2016-v1/RAW-RECO'
+
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+
+config.Data.splitting    = 'LumiBased'
+config.Data.unitsPerJob  = 150  # Since files based, 10 files per job
+config.Data.inputDBS     = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+config.Data.outLFNDirBase  = '/store/user/battilan/NTuplesMuonPOG/ZMu/'
+
+config.section_('Site')
+config.Site.storageSite = 'T3_IT_Bologna'
+
---
+++ Tools/test/crab/crab_SingleMuon_ZMu_Run2016H_cfg.py
@@ -0,0 +1,35 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'SingleMuonRun2016H_ZMu_PromptReco_v2'
+
+config.section_('JobType')
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = '../muonPogNtuples_cfg.py'
+#config.JobType.outputFiles = ['muonNTuple.root']
+config.JobType.pyCfgParams = ['globalTag=80X_dataRun2_Prompt_v9',
+                              'ntupleName=muonPOGNtuple_SingleMuonRun2016H_v2_PromptReco.root',
+                              'hasRaw=True',
+                              'nEvents=-1',
+                              'runOnMC=False',
+                              'hltPathFilter=all',
+                              'minMuPt=45.0',
+                              'minNMu=2'
+               ]
+config.JobType.allowUndistributedCMSSW = True  # To fix cmssw releases
+
+config.section_('Data')
+config.Data.inputDataset = '/SingleMuon/Run2016H-ZMu-PromptReco-v2/RAW-RECO'
+
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+
+config.Data.splitting    = 'LumiBased'
+config.Data.unitsPerJob  = 150  # Since files based, 10 files per job
+config.Data.inputDBS     = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+config.Data.outLFNDirBase  = '/store/user/battilan/NTuplesMuonPOG/ZMu/'
+
+config.section_('Site')
+config.Site.storageSite = 'T3_IT_Bologna'
+
